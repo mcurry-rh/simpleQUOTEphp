51a322d9e5a6f64fae494a8de752523a240ec483
Marc Curry <mcurry@redhat.com>
2019-04-29 23:18:08 -0400

    Comment on where the June 13, 2044 date came from.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <p>"We are animal in our blood and in our skin. We were not born for pavements and escalators but for thunder and mud." -Jay Griffiths<p>
 
 <?php
+/* 78.69yrs = Avg Lifespan of U.S. Male */
 $d1=strtotime("June 13 2044");
 $d2=ceil(($d1-time())/60/60/24);
 echo "You've got " . $d2 ." days left (maybe)...use them wisely.";

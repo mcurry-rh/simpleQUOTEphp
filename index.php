9b044c9f43e73104eb9c33364f8f940ae72ea60d
Marc Curry <mcurry@redhat.com>
2019-04-29 23:00:34 -0400

    Create index.php v1.0.

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head><title>Thunder and Mud</title></head>
+<body>
+
+<p>"We are animal in our blood and in our skin. We were not born for pavements and escalators but for thunder and mud." -Jay Griffiths<p>
+
+<?php
+$d1=strtotime("June 13 2044");
+$d2=ceil(($d1-time())/60/60/24);
+echo "You've got " . $d2 ." days left (maybe)...use them wisely.";
+?>
+
+</body>
+</html>

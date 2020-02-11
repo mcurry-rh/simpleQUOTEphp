f0f9d90b2f1624ca8e18cedc90a33ed105940f4c
Marc Curry <mcurry@redhat.com>
2020-02-11 17:41:41 -0500

    Update index.php
    Punctuation changes.

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 /* 78.69yrs = Avg Lifespan of U.S. Male */
 $d1=strtotime("June 13 2044");
 $d2=ceil(($d1-time())/60/60/24);
-echo "Given the average lifespan of a U.S. male....";
-echo "You've got " . $d2 ." days left (maybe)...use them wisely.";
+echo "Given the average lifespan of a U.S. male...";
+echo "you've got " . $d2 ." days left (maybe). Use them wisely.";
 ?>
 
 </body>
